5b21b30b63a5975abfec152f6f3aef2ca6636e85
daafvader <dawid.malan@gmail.com>
2012-11-22 10:37:31 +0200

    not sure

--- src/project_euler_clj/core.clj
+++ src/project_euler_clj/core.clj
@@ -1,10 +1,9 @@
 (ns project-euler-clj.core)
 (use 'project-euler-clj.problem18_67)
-(use 'clj-time.core)
-(use 'clj-time.format)
-
 
 (defn -main
   "I don't do a whole lot."
   [& args]
   	(println "hello"))
+
+(defn bla [] (println "BLA"))
--- src/project_euler_clj/problem18_67.clj
+++ src/project_euler_clj/problem18_67.clj
@@ -3,9 +3,9 @@
 (def datas (slurp "problem18_data"))
 (def topological (seq (.split #"\s" datas )))
 
-; (def layered (map-indexed vector 
-; 		(map (fn [row] (map-indexed vector 
-; 			(map #(Integer. %) (seq (.split #"\s" row))))) (.split #"\n" datas))))
+(def layered (map-indexed vector 
+ 		(map (fn [row] (map-indexed vector 
+ 			(map #(Integer. %) (seq (.split #"\s" row))))) (.split #"\n" datas))))
 
 
 
